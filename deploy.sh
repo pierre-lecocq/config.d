8cbba6635cb75cebc2d3363f75011eb262ab7fd1
Pierre Lecocq <pierre.lecocq@gmail.com>
2014-04-11 11:41:56 +0200

    first commit

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+curdir=`pwd`
+
+create_dir()
+{
+    if ! [ -L $1 ]; then
+        echo " - creating $1"
+        mkdir $1
+    else
+        echo " - directory $1 already exists"
+    fi
+}
+
+create_link()
+{
+    if ! [ -L $2 ]; then
+        echo " - linking $2"
+        ln -s $1 $2
+    else
+        echo " - link $2 already exists"
+    fi
+}
+
+create_dir "~/.config"
+create_link "$curdir/emacs.d" "~/.emacs.d"
+create_link "$curdir/bin" "~/bin"
+
+# TODO Add i3 & others ...

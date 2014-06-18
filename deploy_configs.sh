7c833cf491067667df8a56ce121a1ab97aa09e5d
Pierre Lecocq <pierre.lecocq@gmail.com>
2014-06-18 18:24:05 +0200

    Switch capslock and control to save my Emacs pinkie

--- deploy_configs.sh
+++ deploy_configs.sh
@@ -24,6 +24,8 @@ curdir=`pwd`
 
 create_link $curdir/gitconfig ~/.gitconfig
 create_link $curdir/bashrc ~/.bashrc
+create_link $curdir/bashrc_profile ~/.bashrc_profile
+create_link $curdir/xmodmap ~/.xmodmap
 create_link $curdir/emacs.d ~/.emacs.d
 create_link $curdir/gtkrc-2.0 ~/.gtkrc-2.0
 

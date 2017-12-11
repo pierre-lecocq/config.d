7178f76bd737ac6946071a3ecc97235e74b51a58
Pierre Lecocq <lecocq@adobe.com>
2017-12-11 13:18:42 +0100

    add option for base branch in git commands

--- common/bin/git-create-branch.sh
+++ common/bin/git-create-branch.sh
@@ -1,11 +1,26 @@
 #!/bin/sh
 
-if [ $# -eq 0 ]; then
+if [ -z "$1"  ]; then
     echo "Please provide a branch name";
     exit 1;
+else
+    name_br="$1"
 fi
 
-git checkout master \
-    && git pull \
-    && git submodule update \
-    && git checkout -b $1
+if [ -z "$2"  ]; then
+    base_br="master"
+else
+    base_br="$2"
+fi
+
+read -p "Create branch \"${name_br}\" from \"${base_br}\"? (y/n): " -n 1 choice
+
+if [ "$choice" == "y" ] || [ "$choice" == "Y" ]; then
+    git checkout ${base_br} \
+        && git pull \
+        && git submodule update \
+        && git checkout -b ${name_br} ${base_br}
+else
+    echo "Abort."
+    exit 1
+fi
